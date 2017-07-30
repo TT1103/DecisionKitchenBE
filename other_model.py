fe3e55c0ffd64c8ca8efce81d58298d8911925a3
coin25 <azzy_coin25@hotmail.com>
2017-07-29 19:49:46 -0700

    Add another model

---
+++ other_model.py
@@ -0,0 +1,48 @@
+import tempfile
+import tensorflow as tf
+import pandas as pd
+
+
+TRAINING_STEPS = 200
+COLUMNS = ["Category_rank", "Price", "Rating", "Rating_Count", "Correctness"]
+LABEL_COLUMN = "label"
+df_train = pd.read_csv("train.txt", names=COLUMNS, skipinitialspace=True)
+df_test = pd.read_csv("test.txt", names=COLUMNS, skipinitialspace=True)
+
+featureList=[tf.contrib.layers.real_valued_column(i) for i in COLUMNS]
+
+
+
+df_train[LABEL_COLUMN] = df_train["Correctness"]
+df_test[LABEL_COLUMN] = df_test["Correctness"]
+
+def input_fn(df):
+  continuous_cols = {i: tf.constant(df[i].values)
+                     for i in COLUMNS}
+  
+  # Merges the two dictionaries into one.
+  feature_cols = dict(continuous_cols.items())
+  # Converts the label column into a constant Tensor.
+  label = tf.constant(df[LABEL_COLUMN].values)
+  # Returns the feature columns and the label.
+  return feature_cols, label
+
+
+def train_input_fn():
+  return input_fn(df_train)
+
+def eval_input_fn():
+  return input_fn(df_test)
+
+model_dir = tempfile.mkdtemp()
+e = tf.contrib.learn.LinearRegressor(feature_columns=featureList, model_dir=model_dir)
+e.fit(input_fn=train_input_fn, steps=TRAINING_STEPS)
+
+
+print(e)
+
+results = e.predict_scores(input_fn=eval_input_fn)
+for key in sorted(results):
+    print(key)
+
+
